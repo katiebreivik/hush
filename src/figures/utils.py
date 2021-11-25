7dc4c67d9442dc2b938431a66a5813d844ffbd65
sthiele483 <sthiele@shaw.ca>
2021-11-25 01:16:42 -0800

    note interaction separation tank in intersep figure from binning, strip utils.py of Lband filename function.

--- src/figures/utils.py
+++ src/figures/utils.py
@@ -26,7 +26,10 @@ def getfiles(kstar1, kstar2, model):
     
     filename_list = []
     for ii in range(len(met_list)):
-        fname = '{}_{}_{}_{}.h5'.format(model, kstar1, kstar2, ii)
+        if kstar1 == '12':
+            fname = '{}_12_10_12_{}.h5'.format(model, ii)
+        else:
+            fname = '{}_{}_{}_{}.h5'.format(model, kstar1, kstar2, ii)
         filename_list.append(fname)
         
     if kstar1 == '12':
@@ -35,42 +38,6 @@ def getfiles(kstar1, kstar2, model):
         label='{}_{}'.format(kstar1, kstar2)
         
     return filename_list, label
-
-
-def Lband_files(kstar1, kstar2, model, var=True):
-    '''
-    Creates list of LISA band filenames for given DWD type.
-    
-    INPUTS
-    ----------------------
-    kstar1 [10, 11, 12]: type of first WD
-    kstar2 [10, 11, 12]: type of second WD
-    model ['fiducial', 'alpha25', 'alpha5', 'q3']: specifies binary evolution assumptions
-    var [bool]: if True, creates keys for FZ, else for F50
-    
-    RETURNS
-    ----------------------
-    files [list, str]: list of keys
-    '''
-    met_list = [0.0001, 0.00015029, 0.00022588, 0.00033948, 0.00051021, 
-                0.00076681, 0.00115245, 0.00173205, 0.00260314, 0.00391233, 
-                0.00587993, 0.0088371, 0.01328149, 0.01996108, 0.03]
-    if var:
-        var_list = [0.4847, 0.4732, 0.4618, 0.4503, 0.4388, 
-                    0.4274, 0.4159, 0.4044, 0.3776, 0.3426, 
-                    0.3076, 0.2726, 0.2376, 0.2027, 0.1677]
-      
-        if kstar1 != '12':
-            files = ['Lband_{}_{}_{}_{}_{}.hdf'.format(kstar1, kstar2, int(met*1e5), int(var*1e5), model) for var, met in zip(var_list, met_list)]
-        else:
-            files = ['Lband_{}_{}_{}_{}.hdf'.format(kstar1, int(met*1e5), int(var*1e5), model) for var, met in zip(var_list, met_list)]
-    else:
-        if kstar1 != '12':
-            files = ['Lband_{}_{}_{}_0.5_{}.hdf'.format(kstar1, kstar2, int(met*1e5), model) for met in met_list]
-        else:
-            files = ['Lband_{}_{}_0.5_{}.hdf'.format(kstar1, int(met*1e5), model) for met in met_list]
-    
-    return keys
 
 
 def get_binfrac_of_Z(Z):
